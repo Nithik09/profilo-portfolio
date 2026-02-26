034285f8b88e563025d4e6f3b58d8189023260d6
Nithik09 <nithikroshan06@gmail.com>
2026-02-25 22:14:01 -0600

    Use roshan_no_bg image for favicon

--- gen-favicon.ps1
+++ gen-favicon.ps1
@@ -1,10 +1,23 @@
+param(
+    [string]$Source = 'public/roshan_no_bg.png',
+    [string]$BaseName = 'roshan_no_bg'
+)
+
 Add-Type -AssemblyName System.Drawing
-$src = Join-Path $PSScriptRoot 'public/roshan.jpeg'
+
+$src = Join-Path $PSScriptRoot $Source
 if (-not (Test-Path $src)) {
     Write-Error "Source not found: $src"
     exit 1
 }
+
+$outDir = Join-Path $PSScriptRoot 'public'
+$out32 = Join-Path $outDir "$BaseName-32.png"
+$out512 = Join-Path $outDir "$BaseName-512.png"
+$outTouch = Join-Path $outDir "$BaseName.png"
+
 $img = [System.Drawing.Image]::FromFile($src)
+
 function Save-Resized {
     param(
         [Parameter(Mandatory)][System.Drawing.Image]$Image,
@@ -20,7 +33,8 @@ function Save-Resized {
     $bmp.Save($Path, [System.Drawing.Imaging.ImageFormat]::Png)
     $bmp.Dispose()
 }
-Save-Resized -Image $img -Width 32 -Height 32 -Path (Join-Path $PSScriptRoot 'public/roshan-32.png')
-Save-Resized -Image $img -Width 512 -Height 512 -Path (Join-Path $PSScriptRoot 'public/roshan-512.png')
+
+Save-Resized -Image $img -Width 32 -Height 32 -Path $out32
+Save-Resized -Image $img -Width 512 -Height 512 -Path $out512
 $img.Dispose()
-Copy-Item (Join-Path $PSScriptRoot 'public/roshan-512.png') (Join-Path $PSScriptRoot 'public/roshan.png') -Force
+Copy-Item $out512 $outTouch -Force
